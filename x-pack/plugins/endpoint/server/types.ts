b4eee4f58cb24495eb9fcae5074add926a5da061
Davis Plumlee <davis.plumlee@elastic.co>
2020-01-21 17:02:59 -0700

    data feeds in from backend but doesnt update

--- x-pack/plugins/endpoint/server/types.ts
+++ x-pack/plugins/endpoint/server/types.ts
@@ -52,27 +52,3 @@ export interface EndpointData {
     };
   };
 }
-
-export interface AlertData {
-  endgame: {
-    data: {
-      file_operation: string;
-      malware_classification: {
-        score: number;
-      };
-    };
-    metadata: {
-      key: string;
-    };
-    timestamp_utc: Date;
-  };
-  host: {
-    hostname: string;
-    ip: string;
-    os: {
-      name: string; // TODO Union types?
-      platform: string;
-      version: string;
-    };
-  };
-}
